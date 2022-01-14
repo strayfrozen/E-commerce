298cef1be867ebd5dd5377d38dc2e6313397dc97
Stray <strayfrozen@yahoo.com>
2022-01-14 13:29:33 -0600

    created model associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,40 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Create associations
+
+
 // Products belongsTo Category
+
+Product.belongsTo(Category, {
+  //unsure on foreign key
+  foreignKey: 'product_id'
+});
 
 // Categories have many Products
 
+Category.hasMany(Product, {
+  //unsure on foreign key
+  foreignKey: 'product_id'
+});
+
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //unsure on as and foreign key
+  as: 'tag',
+  foreignKey: 'product_id'
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  //unsure on as and foreign key
+  as: 'tag',
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
