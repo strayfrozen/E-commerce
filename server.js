c4959666875a75d4beb3c89031bdc023165a9e46
Stray <strayfrozen@yahoo.com>
2022-01-14 15:46:13 -0600

    synced sequilize to the database and server start

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const routes = require('./routes');
+const session = require('express-session');
 // import sequelize connection
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
